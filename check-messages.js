e51bd59987b81aea04abf22011926044c3942465
Seulsul <modsiwho@gmail.com>
2026-08-06 23:24:55 +0900

    Update check-messages.js

--- check-messages.js
+++ check-messages.js
@@ -64,8 +64,9 @@ async function sendNtfy(msg) {
   await fetch('https://ntfy.sh/' + NTFY_TOPIC, {
     method: 'POST',
     headers: {
-  Title: encodeHeader('동시녹음 새 메시지 (' + msg.date + '/' + computeDDay(msg.date) + ')'),
+  Title: encodeHeader('상병 김동영 (' + msg.date + '/' + computeDDay(msg.date) + ')'),
   Priority: 'high',
+  Tags: 'saluting_face',
 },
     body: msg.text,
   });
