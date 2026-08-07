7f974281af65a8e63e889f14fe1a7061f9be9bce
Seulsul <modsiwho@gmail.com>
2026-08-07 11:19:12 +0900

    Update check-messages.js

--- check-messages.js
+++ check-messages.js
@@ -1,7 +1,6 @@
 const { firefox } = require('playwright');
 const fs = require('fs');
 const path = require('path');
-throw new Error('테스트용 강제 실패');
 
 const SITE_URL = 'https://sound-of-memories.smtown.com/';
 const NTFY_TOPIC = process.env.NTFY_TOPIC;
